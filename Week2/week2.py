8b430c2260dcd6da5040ea5a547ae85aa42b3859
Daeye2 <gavrilutageorge@gmail.com>
2024-10-26 11:47:06 +0100

    week 2 if else

---
+++ Week2/week2.py
@@ -0,0 +1,8 @@
+print("Please enter a number")
+
+number = int(input())
+
+if number % 2 == 0:
+    print(f"The entered no {number} is an even number.")
+else:
+    print(f"The entered no {number} is an odd number.")
