caba7ddfaac5fb42b6694db4c8a3c3f0ef8d7142
nick <gavrilutageoge@gmail.com>
2024-11-02 12:17:29 +0000

    for loop

---
+++ Week3/week3.3.py
@@ -0,0 +1,13 @@
+print ("How many Mountains you want to see?")
+
+display = int(input())
+
+for mountain in range(display):
+    print(""""       
+           __
+          /  \\_  
+         /^    \\_
+        /  ^    \\_
+      _/ ^ ^     ^\\_
+     /  ^     ^    \\_ 
+     """)
