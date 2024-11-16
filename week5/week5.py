58998dbc4cbe2653cbc1b0a01c63d35a6024454d
nick <gavrilutageoge@gmail.com>
2024-11-16 09:44:50 +0000

    functions

---
+++ week5/week5.py
@@ -0,0 +1,28 @@
+theList = ["apple", "banana", "cherry", "blueberry","lemon", "strawberry"]
+tropical = ["papaya", "pear", "avocado"]
+print(theList)
+theList[1] = "mango"
+print(theList)
+theList.insert(1,"watermelon")
+print(theList)
+theList.append("orange")
+print(theList)
+
+theList.extend(tropical)
+print(theList)
+
+
+
+
+
+
+
+
+
+
+
+#listOf = ["apple", "banana", "cherry"]
+#if "lemon" in listOf:
+ #   print("yes, 'apple' is in the list")
+#else:
+ #   print("no, 'apple' is not in the list")
