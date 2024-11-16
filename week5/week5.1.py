dbcfc239afa8cbadd380252409a61e2c04c321bb
nick <gavrilutageoge@gmail.com>
2024-11-16 11:58:22 +0000

    week 5 task 1

---
+++ week5/week5.1.py
@@ -0,0 +1,9 @@
+def direction():
+    steps = ["Move Forward", "Move Backward", "Turn Left" , "Turn Right"]
+    return steps
+
+def run_task():
+    print(direction())
+
+if __name__ == "__main__":
+    run_task()
