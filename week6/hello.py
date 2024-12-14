2320d3c7b4026f0a79f06d3e61a6d8a06d37d6e9
nick <gavrilutageoge@gmail.com>
2024-12-14 09:30:12 +0000

    dictionary

---
+++ week6/hello.py
@@ -0,0 +1,12 @@
+print("how many apples should i remove")
+
+remove_apple = int(input())
+no_of_removed_apple = 1
+
+while no_of_removed_apple <= remove_apple:
+    print(f"Apple {no_of_removed_apple} removed")
+    no_of_removed_apple += 1
+
+
+
+    
