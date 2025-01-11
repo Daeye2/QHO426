95e906d913723a6e6e7778d7a5c0ace28218a168
nick <gavrilutageoge@gmail.com>
2025-01-11 12:18:18 +0000

    dictionary

--- week9/week9.py
+++ week9/week9.py
@@ -1,7 +1,9 @@
 import csv
 
+
 records = []
 headings = []
+
 
 def load_data(file_path):
     global records, headings
@@ -15,6 +17,33 @@ def load_data(file_path):
         for row in csv_reader:
             records.append(row)
 
+def display_menu():
+    print("""
+      Please select one of the following options:
+      [1] Display the names of all passengers
+      [2] Display the number of passengers that survived
+      [3] Display the number of passengers per gender
+      [4] Display the number of passengers per age group
+      
+      Please select the option 1-4
+         """)
+    user_input = int(input())
+    return user_input
+
+def display_passengers():
+    print("The names of the passengers are...")
+    for record in records:
+        passenger_name = record[3]
+        print(passenger_name)
+
+
+def display_passenger_names():
+    print("The names of the passengers are...")
+    for record in records:
+        passenger_name = record[3]
+        print(passenger_name)
+
+
 def run():
     global records
     file_path = "titanic.csv"
@@ -22,5 +51,14 @@ def run():
 
     print(f"Successfully loaded {len(records)}")
     print("Done")
+
+    selected_option = display_menu()
+
+    print(f"You have selected {selected_option}")
+
+    if selected_option == 1:
+        display_passenger_names()
+    else:
+        print("Error! Option not recognised!")
 
 run()
