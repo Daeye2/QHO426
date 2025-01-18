67e61105a4aaacaccb983b1f1ec047b6b9e439ba
nick <gavrilutageoge@gmail.com>
2025-01-18 14:48:01 +0000

    CSV reader added

--- project_template/process.py
+++ project_template/process.py
@@ -3,5 +3,12 @@ This module is responsible for processing the data.  It will largely contain fun
 perfrom necessary processes in order to provide the desired result in the desired format.
 It is likely that most sections will require functions to be placed in this module.
 """
+import csv
 
-
+def load_data(filename):
+    data = []
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
