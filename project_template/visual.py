6a12ab878b15c1a55b11847614b465196ddd0fb8
nick <gavrilutageoge@gmail.com>
2025-01-18 14:49:30 +0000

    matplotlib added

--- project_template/visual.py
+++ project_template/visual.py
@@ -2,4 +2,19 @@
 This module is responsible for visualising the data using Matplotlib.
 Any visualisations should be generated via functions in this module.
 """
+from collections import Counter
 
+from matplotlib import pyplot as plt
+
+
+def pie_chart_review_counts(data):
+    # Count reviews for each park
+    park_counts = Counter(review['Branch'] for review in data)
+    labels = list(park_counts.keys())
+    sizes = list(park_counts.values())
+
+    # Plot the pie chart
+    plt.figure(figsize=(8, 8))
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
+    plt.title("Review Counts by Park")
+    plt.show()
